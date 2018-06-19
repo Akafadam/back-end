671b6b0d68e599b3bb01184538f101ff39817725
Ricardo <ricardoch2000@gmail.com>
2018-06-18 23:09:16 -0400

    Fixing forms into html

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,1 +1,9 @@
 from django import forms
+
+class Signin(forms.Form):
+
+	def get_email():
+		email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email address'}))
+
+	def get_password():
+		password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .forms import Signin
 
 class SigninTemplate(TemplateView):
 	template_name = 'myapp/signin.html'
+	initial = {'key': 'value'} 
+	email_class = Signin.get_email
+	password_class = Signin.get_password
 
-	def get(self,request):
-		return render(request,self.template_name)
+	def get(self, request, *args, **kwargs):
+		email = self.email_class(initial=self.initial)
+		password = self.password_class(initial=self.initial)
+		return render(request,self.template_name, {'email': email, 'password': password,})
 
 class SignupTemplate(TemplateView):
 	template_name = 'myapp/signup.html'
