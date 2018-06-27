a47a6ac43d4c0455726fbe18017c650201b6f750
Ricardo <ricardoch2000@gmail.com>
2018-06-27 09:57:07 -0400

    Problem: User validation is not working

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class Signin(forms.ModelForm):
+class Signin(forms.Form):
 	email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email address'}))
 	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
 
